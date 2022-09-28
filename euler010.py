4f732fbae0f87b17dca42993e9eeeba7cd35d9fb
SFNDude123502 <102396379+SFNDude123502@users.noreply.github.com>
2022-09-28 12:51:27 -0400

    Update euler010.py

--- euler010.py
+++ euler010.py
@@ -1,11 +1,6 @@
-def euler10():
-    s=2
-    for i in range(3,2000000,2):
-        ret=True
-        for j in range(3,int((i**0.5)+2),2):
-            if i%j==0:
-                ret=False
-        if ret:
-            s+=i
-    return s
-print(euler10())
+import funcs as f
+s=2
+for i in range(3,2000000,2):
+    if f.isPrime(i):
+        s+=i
+print(s)
