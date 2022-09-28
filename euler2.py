cc73502c7193e5209a704098c4a566547dd9d52b
SFNDude123502 <102396379+SFNDude123502@users.noreply.github.com>
2022-09-28 09:10:21 -0400

    Update euler2.py

--- euler2.py
+++ euler2.py
@@ -1,12 +1,14 @@
+import funcs as f
 def fib(a,b):
     global su
-    print(a)
     _t=a+b
     i=b
     j=_t
     if su<=4000000: 
-        su+=a
+        if f.isEven(a):
+            su+=a    
         fib(i,j)
+        
 su=0
 m=0
 n=1
