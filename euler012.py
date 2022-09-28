28430db369c83223eef44599cfadd9d93d5a38ad
SFNDude123502 <102396379+SFNDude123502@users.noreply.github.com>
2022-09-28 14:08:34 -0400

    Update euler012.py

--- euler012.py
+++ euler012.py
@@ -1,15 +1,16 @@
-def euler12():
-    se=0
+se=0
+temp=0
+for i in range(1,1000000):
+    se+=i
     temp=0
-    for i in range(1,1000000,1):
-        se+=i
-        temp=0
-        for j in range(1,int(se/2)+1):
-            if se%j==0:
+    for j in range(1,int(se**.5)+1):
+        if se%j==0:
+            if se/j>se**.5:
+                temp+=2
+            else: 
                 temp+=1
+    if temp>=200: 
         print("Tri: "+str(i)+" Divis: "+str(temp))
-        if temp>=500:
-            print(se)
-            return i
-
-print(euler12())
+    if temp>=500:
+        print(i, se)
+        break
