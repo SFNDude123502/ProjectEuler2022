8d5c1f36a75319cdb6f308080fbed608e217a771
SFNDude123502 <102396379+SFNDude123502@users.noreply.github.com>
2022-09-28 11:57:23 -0400

    Update euler007.py

--- euler007.py
+++ euler007.py
@@ -1,14 +1,8 @@
-def euler7():
-    lis=[]
-    for i in range(1,1000000,2):
-        pri=True
-        for j in range(3,int((i/2)+2),2):
-            if i%j==0:
-                pri=False
-            else:
-                continue
-        if pri:
-            lis.append(i)
-        if len(lis)>=10001:
-            return lis[10000]
-print(euler7())
+
+c=0
+for i in range(1,1000000,2):
+    if isPrime(i):
+        c+=1
+    if c==10001:
+        print(i)
+        break
