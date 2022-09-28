fb96855a2442a756d15a1dc5c37706b1efa7180b
SFNDude123502 <102396379+SFNDude123502@users.noreply.github.com>
2022-09-28 11:50:10 -0400

    Update euler005-2.py

--- euler005-2.py
+++ euler005-2.py
@@ -1,16 +1,6 @@
-def primeFactorization(nu):
-  _facts=[]
-  while nu%2==0:
-    _facts.append(2)
-  for i in range(3,int(nu**.5)+1,2):
-    if nu%i==0:
-      _facts.append(i)
-      nu/=i
-  if nu>2:
-    _facts.append(nu)
-  return _facts
+import funcs as f
 su=1
 for i in [10,11,13,14,16,17,18,19]:
-  for j in primeFactorization(i):
+  for j in f.primeFactorization(i):
     su*=j
 print(su)
