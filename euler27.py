0cc9d95598271843af18f65711565857343af8ce
SFNDude123502 <102396379+SFNDude123502@users.noreply.github.com>
2022-09-28 09:04:02 -0400

    Update and rename euler27pt1.py to euler27.py

--- euler27.py
+++ euler27.py
@@ -7,7 +7,20 @@ for a in range(-999,1000):
         for n in range(1,11):
             if f.isPrime(f.quadratic(a,b,n)):
                 _t.append(n)
+            else:
+                break
         if len(_t)>=10:
             _contenders.append([a,b])
             print(a,b,len(_t))
-print(_contenders)
+lis = _contenders
+lrg=[0,0]
+lrgc=0
+for i in range(len(lis)):
+    c=1
+    while f.isPrime(f.quadratic(lis[i][0],lis[i][1],c)):
+        c+=1
+    if c>lrgc:
+        lrgc=c
+        lrg[0]=lis[i][0]
+        lrg[1]=lis[i][1]
+print(lrg, lrgc, lrg[0]*lrg[1])
